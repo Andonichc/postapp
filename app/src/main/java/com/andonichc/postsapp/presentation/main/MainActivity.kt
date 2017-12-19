affc9762742dfc64cc18102e29c777dff0b546d0
Andoni Carmona <andonicch@gmail.com>
2017-12-19 20:08:35 +0100

    added base libraries

--- app/src/main/java/com/andonichc/postsapp/presentation/main/MainActivity.kt
+++ app/src/main/java/com/andonichc/postsapp/presentation/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.andonichc.postsapp
+package com.andonichc.postsapp.presentation.main
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.andonichc.postsapp.R
 
 class MainActivity : AppCompatActivity() {
 
