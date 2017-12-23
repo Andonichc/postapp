9c22148e659e5560948712b00cfb39a04f18a3a0
Andoni Carmona <andonicch@gmail.com>
2017-12-23 13:02:48 +0100

    added margin

--- app/src/main/java/com/andonichc/postsapp/presentation/main/MainListAdapter.kt
+++ app/src/main/java/com/andonichc/postsapp/presentation/main/MainListAdapter.kt
@@ -3,9 +3,12 @@ package com.andonichc.postsapp.presentation.main
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import com.andonichc.postsapp.R
 import com.andonichc.postsapp.presentation.main.model.PostPresentationModel
 import com.andonichc.postsapp.presentation.utils.ext.inflate
+import com.andonichc.postsapp.presentation.utils.ext.setAllMargins
+import com.andonichc.postsapp.presentation.utils.ext.toDp
 import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.item_post.view.*
 
@@ -13,8 +16,16 @@ import kotlinx.android.synthetic.main.item_post.view.*
 abstract class MainListAdapter(private val items: List<PostPresentationModel>) :
         RecyclerView.Adapter<MainListAdapter.PostViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder =
-            PostViewHolder(parent.inflate(R.layout.item_post, false))
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostViewHolder {
+
+        val view = parent.inflate(R.layout.item_post, false)
+
+        (view.layoutParams as RecyclerView.LayoutParams)
+                .setAllMargins(4F.toDp(view.context))
+
+        return PostViewHolder(view)
+    }
+
 
     override fun onBindViewHolder(holder: PostViewHolder, position: Int) =
             holder.bind(items[position])
--- app/src/main/java/com/andonichc/postsapp/presentation/utils/ext/ViewExt.kt
+++ app/src/main/java/com/andonichc/postsapp/presentation/utils/ext/ViewExt.kt
@@ -1,6 +1,9 @@
 package com.andonichc.postsapp.presentation.utils.ext
 
+import android.content.Context
 import android.support.annotation.LayoutRes
+import android.util.TypedValue
+import android.util.TypedValue.COMPLEX_UNIT_DIP
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,3 +11,14 @@ import android.view.ViewGroup
 
 fun ViewGroup.inflate(@LayoutRes layout: Int, attachToRoot: Boolean): View =
         LayoutInflater.from(context).inflate(layout, this, attachToRoot)
+
+
+fun Float.toDp(context: Context) =
+        TypedValue.applyDimension(COMPLEX_UNIT_DIP, this, context.resources.displayMetrics)
+
+
+fun ViewGroup.MarginLayoutParams.setAllMargins(marginInPx: Int) =
+        this.setMargins(marginInPx, marginInPx, marginInPx, marginInPx)
+
+fun ViewGroup.MarginLayoutParams.setAllMargins(marginInPx: Float) =
+    this.setAllMargins(marginInPx.toInt())
